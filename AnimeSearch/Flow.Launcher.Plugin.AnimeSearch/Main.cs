e6942bbc11c1165baa7fb1baec1000c0983bb0e7
Mayowa <70532397+MayowaAk@users.noreply.github.com>
2022-07-02 16:13:14 +0100

    Edited plugin.json/ Added Search functionality

--- AnimeSearch/Flow.Launcher.Plugin.AnimeSearch/Main.cs
+++ AnimeSearch/Flow.Launcher.Plugin.AnimeSearch/Main.cs
@@ -3,16 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using JikanDotNet;
 
 namespace Flow.Launcher.Plugin.AnimeSearch
 {
     public class AnimeSearch : IPlugin
     {
         private PluginInitContext _context;
-
+        public IJikan jikan;
         public void Init(PluginInitContext context)
         {
             _context = context;
+            jikan = new Jikan();
         }
 
         public List<Result> Query(Query query)
@@ -23,7 +25,11 @@ namespace Flow.Launcher.Plugin.AnimeSearch
                 SubTitle = $"Query: {query.Search}",
                 Action = c =>
                 {
-                    _context.API.ShowMsg("Test");
+                    var searchResults = jikan.SearchAnimeAsync(query.Search);
+                    foreach (var searchResult in searchResults.Result.Data)
+                    {
+                        _context.API.ShowMsg(searchResult.Title);
+                    }
                     return true;
                 }
             };
